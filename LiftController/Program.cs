091d59b433e212bd43d917800c4f2f079f219a77
Haru-Tachibana <paloma030415@gmail.com>
2025-10-14 16:11:54 +0100

    finished lift controller

--- LiftController/Program.cs
+++ LiftController/Program.cs
@@ -11,11 +11,32 @@
             floorNames[3] = "Third";
             floorNames[4] = "Fourth";
 
+            Console.WriteLine();
             Console.WriteLine("The available floors are: ");
-            for (int i = 0; i <= floorNames.Length; i++)
+            for (int i = 0; i < floorNames.Length; ++i)
             {
-                Console.WriteLine($"{i + 1}  {floorNames[i]}");
+                Console.WriteLine($"{i}  {floorNames[i]}");
             }
+
+            Console.WriteLine();
+            int pickedFloor;
+            while (true)
+            {
+                Console.Write("Choose a floor number: ");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out pickedFloor) && pickedFloor >= 0 && pickedFloor <= 4)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please enter an integer between 0 and 4.");
+                }
+            }
+
+            Console.WriteLine($"You have chosen the {floorNames[pickedFloor]} Floor.");
+            Console.WriteLine();
         }
     }
 }
