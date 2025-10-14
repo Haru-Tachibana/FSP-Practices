7878cd8246ad1bf146d706a875c66d3bf8b160d8
Yuyang Wang <N1373616@my.ntu.ac.uk>
2025-10-14 14:30:51 +0100

    added lift controller

---
+++ LiftController/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LiftController
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] floorNames = new string[5];
+            floorNames[0] = "Ground";
+            floorNames[1] = "First";
+            floorNames[2] = "Second";
+            floorNames[3] = "Third";
+            floorNames[4] = "Fourth";
+
+            Console.WriteLine("The available floors are: ");
+            for (int i = 0; i <= floorNames.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}  {floorNames[i]}");
+            }
+        }
+    }
+}
